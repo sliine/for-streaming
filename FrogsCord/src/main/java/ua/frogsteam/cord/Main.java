5e97ded515ff99e00652a63e8d523d6cc249e170
sline_gg <dimka.kosuhin@gmail.com>
2021-02-21 20:34:40 +0300

    deleted tocken and create new variante

--- FrogsCord/src/main/java/ua/frogsteam/cord/Main.java
+++ FrogsCord/src/main/java/ua/frogsteam/cord/Main.java
@@ -12,7 +12,7 @@ import javax.security.auth.login.LoginException;
 public class Main {
 
     public static void main(String[] args) throws LoginException {
-        JDA jda = JDABuilder.createDefault("Nzk0ODAyNjcyMTU4Mzc1OTU2.X_AHqw.UXf0gbExVTUYQ7D0wvZN5UC4Jm4").build();
+        JDA jda = JDABuilder.createDefault("").build();
         jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
         jda.getPresence().setActivity(Activity.watching("Help for bots .!help"));
         jda.addEventListener(new CommandInfo());
--- FrogsCord/src/main/java/ua/frogsteam/cord/commands/admin/CommandClear.java
+++ FrogsCord/src/main/java/ua/frogsteam/cord/commands/admin/CommandClear.java
@@ -2,24 +2,62 @@ package ua.frogsteam.cord.commands.admin;
 
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.MessageHistory;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.util.Collection;
+import java.util.List;
 
 public class CommandClear extends ListenerAdapter {
 
     @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        Message msg = event.getMessage();
-        if (msg.getContentRaw().equals(".!clear")) {
-            MessageChannel channel = event.getChannel();
-            event.getMessage().getChannel().sendMessage("test");
-
+    public void onMessageReceived(MessageReceivedEvent e){
+        Message objMsg = e.getMessage();
+        String[] args = objMsg.getContentRaw().split("");
+        if (args[0].equalsIgnoreCase(("!gg"))){
+            if (args.length <= 2){
+            }
+            else {
+                e.getMessage().delete().queue();
+                boolean target = e.getTextChannel().delete().equals(objMsg);
+                purgeMessages(target, Integer.parseInt(args[2]));
+            }
         }
     }
 
-    private Collection<Message> onGuildMessageDelete() {
-        return null;
+    private void purgeMessages(boolean target, int i) {
+        MessageChannel channel = null;
+        MessageHistory history = new MessageHistory(channel);
+        List<Message> msgs;
+
+        int numberofMessages = 0;
+        msgs = history.retrievePast(numberofMessages).complete();
+        channel.deleteMessageById(String.valueOf(msgs)).queue();
     }
+
+    /*public void onGuildReceived(GuildMessageReceivedEvent event) {
+        Message objMsg = event.getMessage();
+        String[] args = objMsg.getContentRaw().split("");
+        if(args[0].equalsIgnoreCase( "!purge")) {
+            if(args.length <= 2) {
+                //TODO
+            }else {
+                event.getMessage().delete().queue();
+                TextChannel target = event.getMessage().getMentionedChannels().get(0);
+                purgeMessages(target, Integer.parseInt(args[2]));
+            }
+        }
+    }
+    private void purgeMessages(TextChannel channel, int numberofMessages) {
+        MessageHistory history = new MessageHistory(channel);
+        List<Message> msgs;
+
+        msgs = history.retrievePast(numberofMessages).complete();
+        channel.deleteMessages(msgs).queue();
+
+    }*/
 }

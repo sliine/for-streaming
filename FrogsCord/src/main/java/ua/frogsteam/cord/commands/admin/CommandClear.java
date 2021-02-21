0e15e2fd9777b5f4dd5d6f050c9cc85b78bf482e
sline_gg <dimka.kosuhin@gmail.com>
2021-02-21 22:44:49 +0300

    fix command clear

--- FrogsCord/src/main/java/ua/frogsteam/cord/commands/admin/CommandClear.java
+++ FrogsCord/src/main/java/ua/frogsteam/cord/commands/admin/CommandClear.java
@@ -16,48 +16,12 @@ public class CommandClear extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent e){
-        Message objMsg = e.getMessage();
-        String[] args = objMsg.getContentRaw().split("");
-        if (args[0].equalsIgnoreCase(("!gg"))){
-            if (args.length <= 2){
-            }
-            else {
-                e.getMessage().delete().queue();
-                boolean target = e.getTextChannel().delete().equals(objMsg);
-                purgeMessages(target, Integer.parseInt(args[2]));
-            }
+        Message msg = e.getMessage();
+        if (msg.getContentRaw().equals("!clear")) {
+            MessageChannel channel = e.getChannel();
+            channel.sendMessage("deleted").queue(response -> {
+                channel.purgeMessages(e.getMessage()).clear();
+            });
         }
     }
-
-    private void purgeMessages(boolean target, int i) {
-        MessageChannel channel = null;
-        MessageHistory history = new MessageHistory(channel);
-        List<Message> msgs;
-
-        int numberofMessages = 0;
-        msgs = history.retrievePast(numberofMessages).complete();
-        channel.deleteMessageById(String.valueOf(msgs)).queue();
-    }
-
-    /*public void onGuildReceived(GuildMessageReceivedEvent event) {
-        Message objMsg = event.getMessage();
-        String[] args = objMsg.getContentRaw().split("");
-        if(args[0].equalsIgnoreCase( "!purge")) {
-            if(args.length <= 2) {
-                //TODO
-            }else {
-                event.getMessage().delete().queue();
-                TextChannel target = event.getMessage().getMentionedChannels().get(0);
-                purgeMessages(target, Integer.parseInt(args[2]));
-            }
-        }
-    }
-    private void purgeMessages(TextChannel channel, int numberofMessages) {
-        MessageHistory history = new MessageHistory(channel);
-        List<Message> msgs;
-
-        msgs = history.retrievePast(numberofMessages).complete();
-        channel.deleteMessages(msgs).queue();
-
-    }*/
 }
